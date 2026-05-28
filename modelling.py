d2a14a35e0e791ca80e5a4adc1453c6443ede893
Kajeenonchalant <permatayogi180@gmail.com>
2026-05-28 18:07:31 +0700

    fix nested mlflow run

--- modelling.py
+++ modelling.py
@@ -6,13 +6,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
+# aktifkan autolog
 mlflow.sklearn.autolog()
 
+# load dataset
 df = pd.read_csv("titanic_preprocessing.csv")
 
+# pisahkan fitur dan target
 X = df.drop("Survived", axis=1)
 y = df["Survived"]
 
+# split data
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
@@ -20,21 +24,25 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state=42
 )
 
-mlflow.set_experiment("Titanic_Experiment")
+# nested=True agar tidak bentrok dengan mlflow run .
+with mlflow.start_run(nested=True):
 
-with mlflow.start_run():
-
+    # model
     model = RandomForestClassifier(
         n_estimators=100,
         random_state=42
     )
 
+    # training
     model.fit(X_train, y_train)
 
+    # prediksi
     y_pred = model.predict(X_test)
 
+    # evaluasi
     acc = accuracy_score(y_test, y_pred)
 
+    # log metric manual
     mlflow.log_metric("accuracy", acc)
 
     print("Accuracy:", acc)
